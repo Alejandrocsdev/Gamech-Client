e2366b7132125250ec4ed7046b2e5b1320837fcf
Alejandrocsdev <alejandrocsdev@gmail.com>
2025-11-30 23:56:00 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// CSS
+import './assets/css/font.css';
+import './assets/css/global.css';
+// Libraries
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Public Pages
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+// CSS Module
+import S from './style.module.css';
+
+function Home() {
+  return (
+    <main className={S.main}>
+      <h1>Home Page</h1>
+    </main>
+  );
+}
+
+export default Home;
